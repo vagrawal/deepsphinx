a3607309a6f687b960e7450367fd098cd92d4635
Vishal Agrawal <vishal4556@gmail.com>
2017-08-06 01:07:56 +0530

    Fix a mistake

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='deepshinx',
     version='0.1',
-    packages=['deepshinx'],
-    scripts=['bin/deepshinx_train'],
+    packages=['deepsphinx'],
+    scripts=['bin/deepsphinx_train'],
     description='Trainer',
     # Replace with tensorflow-gpu if using GPU
     install_requires=['python_speech_features',
-                      'tensorflow==1.3',
+                      'tensorflow==1.2.1',
                       'scipy',
                       'pysoundfile',
                       'openfst'])
