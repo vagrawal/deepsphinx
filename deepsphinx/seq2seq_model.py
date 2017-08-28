90c5fa2557dee509fb603a89f41438739928eea9
Vishal Agrawal <vishal4556@gmail.com>
2017-08-29 00:53:24 +0530

    Make code python3 compatible

--- deepsphinx/seq2seq_model.py
+++ deepsphinx/seq2seq_model.py
@@ -56,7 +56,7 @@ def encoding_layer(
                     "conv_filter_{}".format(layer),
                     shape = [2, 2 * FLAGS.rnn_size, FLAGS.rnn_size])
                 rnn_inputs = tf.nn.conv1d(rnn_inputs, filt, 2, 'SAME')
-                input_lengths = (input_lengths + 1) / 2
+                input_lengths = (input_lengths + 1) // 2
     # Join outputs since we are using a bidirectional RNN
     enc_output = tf.concat(enc_output, 2)
 
