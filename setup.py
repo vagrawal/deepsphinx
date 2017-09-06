47b1bf4312c972691cdaf4777b1ae2959ba1b705
Vishal Agrawal <vishal4556@gmail.com>
2017-09-06 22:52:22 +0530

    Make openfst an optional dependency

--- setup.py
+++ setup.py
@@ -11,5 +11,7 @@ setup(
     install_requires=['python_speech_features',
                       'tensorflow==1.3.0',
                       'scipy',
-                      'pysoundfile',
-                      'openfst'])
+                      'pysoundfile'],
+    extras_require = {
+        'openfst':  ['openfst'],
+},)
